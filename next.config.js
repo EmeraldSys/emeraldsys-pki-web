dd1ba79ad8787ddc2d24c76f2a4787307b634467
Ryan Omasta <ryand@emeraldsys.xyz>
2022-09-16 17:46:40 -0600

    Add rewrites to next.config.js

--- next.config.js
+++ next.config.js
@@ -2,4 +2,18 @@
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
+  rewrites: async () => {
+    return [
+      {
+        "source": "/:path*",
+        "has": [
+          {
+            "type": "host",
+            "value": "auth.pki.emeraldsys.xyz"
+          }
+        ],
+        "destination": "/auth/:path*"
+      }
+    ]
+  }
 }
