3dcf3f2fbabd104773fc745393b5474ff20bc52b
Ryan Omasta <ryand@emeraldsys.xyz>
2022-09-17 02:47:39 -0600

    Change beforeFiles to afterFiles

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
   swcMinify: true,
   rewrites: async () => {
     return {
-      beforeFiles: [
+      afterFiles: [
         {
           "source": "/:path*",
           "has": [
