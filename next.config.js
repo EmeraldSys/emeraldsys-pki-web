e11eac4f19535935bdbebb368b36cf4e524e74b5
Ryan Omasta <ryand@emeraldsys.xyz>
2022-09-16 22:42:47 -0600

    Add beforeFiles to rewrites

--- next.config.js
+++ next.config.js
@@ -3,17 +3,19 @@ module.exports = {
   reactStrictMode: true,
   swcMinify: true,
   rewrites: async () => {
-    return [
-      {
-        "source": "/:path*",
-        "has": [
-          {
-            "type": "host",
-            "value": "auth.pki.emeraldsys.xyz"
-          }
-        ],
-        "destination": "/auth/:path*"
-      }
-    ]
+    return {
+      beforeFiles: [
+        {
+          "source": "/:path*",
+          "has": [
+            {
+              "type": "host",
+              "value": "auth.pki.emeraldsys.xyz"
+            }
+          ],
+          "destination": "/auth/:path*"
+        }
+      ]
+    };
   }
 }
